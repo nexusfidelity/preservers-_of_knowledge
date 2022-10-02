55343128f402eb1c12d297e94f5b8b281cba9f80
nexusfidelity <fidelivancdo@gmail.com>
2022-10-02 11:05:48 +0800

    added general info

--- AI_App.py
+++ AI_App.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import pandas as pd
+from PIL import Image
 #import numpy as np
 
 #nltk initializers
@@ -23,6 +24,9 @@ import pandas as pd
 import texthero as hero
 from texthero import preprocessing
 
+pic2 = Image.open('images/2.png')
+pic3 = Image.open('images/3.jpg')
+
 st.set_page_config(
     page_title="Preservers of Knowledge",
     page_icon="🤖",
@@ -36,7 +40,78 @@ st.title('NASA space app challenge')
 sidebar = st.sidebar.radio('App Navigation', ['General Info','Project Demo'])
 
 if sidebar=='General Info':
-    st.header('General Info')
+    st.subheader('High-level Project Summary')
+    
+    st.write('''
+             <p>Project Minerva is a database search engine that use Artificial Intelligence to enhance search queries of users by aiding users in filtering down by topic, subject category, named entities, etc...</p>
+             <br>
+             <p>accessing the NTRS database can be a daunting task with the bombardment of information it provides. Project Minerva simplifies the use access of the database by enhancing the logical behind the search for journals. It uses the NTRS API to access the data and let project Minerva process the information need by the user.</p>
+             ''',unsafe_allow_html=True)
+    
+    st.subheader('Detailed Project Description')
+             
+    st.write('''
+             <p>project Minerva uses AI tech especially NLP techniques to narrow/filter search queries in the NTRS database. It identifies named entities of the search result .</p>
+             <br>
+             <p>Users will first select a topic of their choosing and project Minerva collects the necessary abstracts found in the NTRS server.</p>
+             <br>
+             <p>After filtering down the abstracts, the user can opt in to use an AI to search for named entities in the search result.</p>
+             <br>
+             <p>the AI will identify the words inside the search result and look for entities that the user looks for.</p>
+             <br>
+             <p>this are the options the user can target for the AI to search for:</p>
+             <ul>
+             <li>PERSON: People, including fictional.</li>
+             <li>NORP: Nationalities or religious or political groups.</li>
+             <li>FAC: Buildings, airports, highways, bridges, etc.</li>
+             <li>ORG : Companies, agencies, institutions, etc.</li>
+             <li>GPE: Countries, cities, states.</li>
+             <li>LOC: Non-GPE locations, mountain ranges, bodies of water.</li>
+             <li>PRODUCT: Objects, vehicles, foods, etc. (Not services.)</li>
+             <li>EVENT: Named hurricanes, battles, wars, sports events, etc.</li>
+             <li>WORK_OF_ART: Titles of books, songs, etc.</li>
+             <li>LAW: Named documents made into laws.</li>
+             <li>LANGUAGE: Any named language.</li>
+             <li>DATE: Absolute or relative dates or periods.</li>
+             <li>TIME: Times smaller than a day.</li>
+             <li>PERCENT: Percentage, including ”%“.</li>
+             <li>MONEY: Monetary values, including unit.</li>
+             <li>QUANTITY: Measurements, as of weight or distance.</li>
+             <li>ORDINAL: “first”, “second”, etc.</li>
+             <li>CARDINAL: Numerals that do not fall under another type.</li>
+             </ul>
+             ''',unsafe_allow_html=True)
+    
+    st.subheader('Space Agency Data')
+    
+    st.write('''
+         <p>The NASA STI Repository (also known as the NASA Technical Reports Server (NTRS))</p>
+         <br>
+         <p>We access the database to look for scientific journals for our studies and researches.</p>
+         <br>
+         <a href="https://ntrs.nasa.gov/">link</a>
+         ''',unsafe_allow_html=True)
+         
+    st.image(pic2)
+    
+    st.write('''
+     <br>
+     ''',unsafe_allow_html=True)
+    
+    st.subheader('Hackathon Journey')
+    
+    st.image(pic3)
+    
+    st.write('''
+     <br>
+     ''',unsafe_allow_html=True)
+    st.subheader('References')
+    
+    st.write('''
+     <p>NTRS API: <a href="https://ntrs.nasa.gov/api/openapi/">link</a></p>
+     <p>streamlit docs: <a href="https://docs.streamlit.io/library/api-reference">link</a></p>
+     ''',unsafe_allow_html=True)
+    
 if sidebar=='Project Demo':
     st.header('Project Minerva')
     df = pd.DataFrame()
